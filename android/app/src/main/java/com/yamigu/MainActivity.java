dd56e067dfdf6606930a4d3bf7f8e5d0cae9e321
kimwoojin <94rladnwls@naver.com>
2020-02-06 01:45:33 +0900

    SplashScreen: implemented splash pages

--- android/app/src/main/java/com/yamigu/MainActivity.java
+++ android/app/src/main/java/com/yamigu/MainActivity.java
@@ -1,15 +1,16 @@
 package com.yamigu;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
 public class MainActivity extends ReactActivity {
- @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);
-        super.onCreate(savedInstanceState);
-    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
