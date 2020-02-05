bf4bdf1c1db5d7eba416dfe5c3894e6f803c631f
khc196 <kh146@gmail.com>
2020-02-05 18:10:13 +0900

    ChattingListScreen: add marginTop 2 to ChattingPreview's unread count label

--- src/components/ChattingListScreen/ChattingPreview.js
+++ src/components/ChattingListScreen/ChattingPreview.js
@@ -49,6 +49,7 @@ const styles = StyleSheet.create({
     backgroundColor: palette.red,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 2,
   },
   chatPreview: {
     borderBottomWidth: 0,
