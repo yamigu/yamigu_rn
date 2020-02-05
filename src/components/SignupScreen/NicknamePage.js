59b1e6d73e9a1affcbbb931966c42e1ba9528230
khc196 <kh146@gmail.com>
2020-02-05 23:31:51 +0900

    SignupScreen: Implement inner pages

--- src/components/SignupScreen/NicknamePage.js
+++ src/components/SignupScreen/NicknamePage.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
 import {CustomTextMedium, CustomTextRegular} from '../common/CustomText';
 import palette from '~/lib/styles/palette';
-import {Input, Form, Item} from 'native-base';
+import {Item, Input} from 'native-base';
 
-const NicknamePage = ({params}) => (
-  <View style={styles.root}>
-    <CustomTextMedium size={20} color={palette.black}>
-      닉네임을 작성해주세요
-    </CustomTextMedium>
-    <CustomTextRegular size={16} color={palette.gray}>
-      *최대 6글자까지 가능해요!
-    </CustomTextRegular>
-    <Form>
-      <Item>
-        <Input placeholder="한글, 영문, 숫자만 입력 가능"></Input>
+const deviceWidth = Dimensions.get('window').width;
+const NicknamePage = ({params}) => {
+  const [text, setText] = useState('');
+  return (
+    <View style={styles.root}>
+      <CustomTextMedium size={20} color={palette.black}>
+        닉네임을 작성해주세요
+      </CustomTextMedium>
+      <CustomTextRegular size={16} color={palette.gray}>
+        *최대 6글자까지 가능해요!
+      </CustomTextRegular>
+      <Item style={styles.item}>
+        <Input
+          style={styles.input}
+          placeholder="한글, 영문, 숫자만 입력 가능"
+          onChangeText={value => setText({value})}
+          value={text}
+          selectionColor={palette.orange[0]}
+          placeholderTextColor={palette.nonselect}
+        />
+        <View style={styles.inputRightView}>
+          <CustomTextRegular size={9} color={palette.blue}>
+            사용 가능합니다.
+          </CustomTextRegular>
+        </View>
       </Item>
-    </Form>
-  </View>
-);
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  root: {},
+  root: {
+    backgroundColor: palette.default_bg,
+    flexDirection: 'column',
+  },
+  item: {
+    borderBottomWidth: 1,
+    borderColor: palette.blue,
+    marginTop: 16,
+  },
+  input: {
+    fontSize: 14,
+    textAlignVertical: 'bottom',
+    lineHeight: 14,
+    fontFamily: 'NotoSansCJKkr-Regular',
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginBottom: 0,
+    color: palette.black,
+  },
+  inputRightView: {
+    flexDirection: 'column',
+    alignSelf: 'flex-end',
+    alignItems: 'flex-end',
+  },
 });
 export default NicknamePage;
