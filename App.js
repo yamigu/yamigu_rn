a419be892b2779c5c756b61bfab44bc7f6316edb
kimwoojin <94rladnwls@naver.com>
2020-02-06 16:23:45 +0900

    App.js: implemented function for iPhone splash

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './Navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <SafeAreaProvider>
