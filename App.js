69d02cfa194b3cd7eb4157418f29a9da0fd9d55e
kimwoojin <94rladnwls@naver.com>
2020-02-06 16:14:10 +0900

    App.js: implemented function for iPhon splash

--- App.js
+++ App.js
@@ -3,7 +3,13 @@ import {View} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './Navigation';
 
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
+  useEffect(() => {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <SafeAreaProvider>
